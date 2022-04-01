6f82cce95ea77f4be60cc1993c0a73f8b5943de6
tashfiaislamistila <tashfia.cse@gmail.com>
2022-04-01 14:07:12 -0700

    conditional rendering commit

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -5,6 +5,8 @@ const Cart = ({ cart, handleRemoveFromCart }) => {
     //Conditional rendering options
     //1. element variable
     //2. ternary operator condition ? true : false
+    //3. && operator  (shorthand)
+    //4. ||
     let command;
     if (cart.length === 0) {
         command = <div>
@@ -27,8 +29,15 @@ const Cart = ({ cart, handleRemoveFromCart }) => {
                     <button onClick={() => handleRemoveFromCart(TShirt)}>X</button>
                 </p>)
             }
+            {cart.length === 0 || <p className='orange'>Yay you are buying!!</p>}
+            {cart.length === 3 && <div className='orange'>
+                <h3>Trigonal</h3>
+                <p>Tin jon ke gift diba ?</p>
+            </div>
+            }
             {command}
             {cart.length !== 4 ? <p>Keep adding</p> : <button>Remove All</button>}
+            {cart.length === 4 && <button className='orange'>Review Order</button>}
         </div>
     );
 };
